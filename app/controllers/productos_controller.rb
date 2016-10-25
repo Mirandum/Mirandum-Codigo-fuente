45ff35b293c8dc7c8ebdc99d35a18a4f0d263c69
Jonatan <japarrat@unal.edu.co>
2016-10-25 16:13:07 -0500

    Funciona crear producto

--- app/controllers/productos_controller.rb
+++ app/controllers/productos_controller.rb
@@ -6,4 +6,19 @@ class ProductosController < ApplicationController
 	def show
 		@producto = Producto.find(params[:id])
 	end
+
+	def new
+		@producto = Producto.new
+	end
+
+	def create
+		@producto = Producto.new(referencia:  params[:producto][:referencia], 
+			descripcion: params[:producto][:descripcion],
+			precio: params[:producto][:precio],
+			color: params[:producto][:color],
+			devuelto: params[:producto][:false],
+			imagen: params[:producto][:imagen])
+		@producto.save
+		redirect_to @producto
+	end
 end
